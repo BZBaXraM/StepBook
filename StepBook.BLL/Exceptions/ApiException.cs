0a83502b2c7e51cc10bc542da47c390967bd0755
Bahram <baxram1997007@gmail.com>
2024-12-13 15:00:00 +0400

    update...

--- StepBook.BLL/Exceptions/ApiException.cs
+++ StepBook.BLL/Exceptions/ApiException.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace StepBook.BLL.Exceptions;
 
 public class ApiException(int statusCode, string message, string? details)
@@ -5,4 +7,16 @@ public class ApiException(int statusCode, string message, string? details)
     public int StatusCode { get; set; } = statusCode;
     public string Message { get; set; } = message;
     public string? Details { get; set; } = details;
+}
+
+public class LoginException(string message, string? details = null)
+    : ApiException((int)HttpStatusCode.Unauthorized, message, details);
+
+public class RegisterException(string message, string? details = null)
+    : ApiException((int)HttpStatusCode.BadRequest, message, details);
+
+public class RegisterResponse(bool success, string? message = null)
+{
+    public bool Success { get; set; } = success;
+    public string? Message { get; set; } = message;
 }
